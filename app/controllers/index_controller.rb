b28b58014c4bef712c164b4c4d970475114385f2
masataka michikawa <masataaaka3@icloud.com>
2021-02-13 12:23:15 +0000

    first commit

---
+++ app/controllers/index_controller.rb
@@ -0,0 +1,15 @@
+class IndexController < ApplicationController
+  def new
+  end
+
+  def show
+  end
+
+  def edit
+  end
+  def update
+  end
+
+  def edit
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  get 'index/new'
+  get 'index/show'
+  get 'index/edit'
+  get 'index/update'
+  get 'index/edit'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
