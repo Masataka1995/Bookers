754e2aef43e4bdcb968f66eaa49a77e54bf7bf3d
masataka michikawa <masataaaka3@icloud.com>
2021-02-13 15:50:42 +0000

    makeNewIndex

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
-  get 'index/new'
-  get 'index/show'
-  get 'index/edit'
-  get 'index/update'
-  get 'index/edit'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get '/' => 'homes#top'
+  resources :books
 end
