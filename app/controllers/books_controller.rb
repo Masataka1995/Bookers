f54f8b8a323675950b63829104c5a269133bcb58
masataka <hanayoriinko@gmail.com>
2021-02-14 20:18:20 +0900

    finishBookers

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,21 +5,32 @@ class BooksController < ApplicationController
     @books = Book.all
   end
 
-
-  def show
-  end
-
-  def edit
-  end
-  def update
-  end
-
-  def edit
-  end
-
   def create
     book = Book.new(book_params)
     book.save
+    redirect_to book_path
+  end
+
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+
+  def update
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    redirect_to books_path
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    flash[:notice] = 'Book was successfully destroyed.'
     redirect_to books_path
   end
 
